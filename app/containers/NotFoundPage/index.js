02b1cfb174efa9a1337c8711c36354c6dd1f3ccc
Moritz Lang <mlang.mmt-b2016@fh-salzburg.ac.at>
2019-05-05 01:39:49 +0200

    feat(NotFoundPage): render random animal emoji

--- app/containers/NotFoundPage/index.js
+++ app/containers/NotFoundPage/index.js
@@ -9,12 +9,23 @@ import React from 'react'
 
 import { Wrapper, Text } from './Styles'
 
-export default function NotFound() {
-  return (
-    <Wrapper>
-      <Text>
-        Sorry, page not found 🐿.
-      </Text>
-    </Wrapper>
-  )
+
+export class NotFoundPage extends React.PureComponent {
+  
+  getRandomAnimalEmoji = () => {
+    const animals = ['🐿', '🐢', '🐶', '🐱', ' 🦆', '🦄']
+    return animals[Math.floor(Math.random() * animals.length)]
+  }
+
+  render() {
+    return (
+      <Wrapper>
+        <Text>
+          Sorry, page not found {this.getRandomAnimalEmoji()}.
+        </Text>
+      </Wrapper>
+    )
+  }
 }
+
+export default NotFoundPage
