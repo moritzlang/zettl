34efbcacb9fdb0ee22d547d02f59acfbd0a787a5
Moritz Lang <mlang.mmt-b2016@fh-salzburg.ac.at>
2019-05-04 22:55:16 +0200

    feat(shadows): add constants for box shadows

---
+++ app/styles/shadows.js
@@ -0,0 +1,3 @@
+export const BOX_SHADOW_DEFAULT = "0 2px 4px 0 rgba(0,0,0,0.10)"
+export const BOX_SHADOW_MEDIUM = "0 4px 8px 0 rgba(0,0,0,0.12), 0 2px 4px 0 rgba(0,0,0,0.08)"
+export const BOX_SHADOW_LARGE = "0 15px 30px 0 rgba(0,0,0,0.11), 0 5px 15px 0 rgba(0,0,0,0.08)"
