28963ff0bec9fc82249cda7102f582d35a0bea0f
Moritz Lang <mlang.mmt-b2016@fh-salzburg.ac.at>
2019-05-04 22:56:34 +0200

    feat(colors): add constants for colors

---
+++ app/styles/colors.js
@@ -0,0 +1,46 @@
+// Primary
+export const BLUE_PRIMARY = "#2BB0ED"
+
+// Icons
+export const ICON_PRIMARY = "#A5B3BB"
+export const ICON_SECONDARY = "#0D2B3E"
+
+// Supporting
+export const RED_050 = "#FFE3E3"
+export const RED_100 = "#FFBDBD"
+export const RED_200 = "#FF9B9B"
+export const RED_300 = "#F86A6A"
+export const RED_400 = "#EF4E4E"
+export const RED_PRIMARY = "#E12D39"
+export const RED_600 = "#CF1124"
+export const RED_700 = "#AB091E"
+export const RED_800 = "#8A041A"
+export const RED_900 = "#610316"   
+
+export const GREEN_050 = "#EFFCF6"
+export const GREEN_100 = "#C6F7E2"
+export const GREEN_200 = "#8EEDC7"
+export const GREEN_300 = "#65D6AD"
+export const GREEN_400 = "#3EBD93"
+export const GREEN_500 = "#27AB83"
+export const GREEN_600 = "#199473"
+export const GREEN_700 = "#147D64"
+export const GREEN_800 = "#0C6B58"
+export const GREEN_900 = "#014D40"
+
+// Neutral
+export const BACKGROUND_COLOR = '#F9F7F3'
+
+export const WHITE = "#FFFFFF"
+
+export const GRAY_050 = "#F7F7F7"
+export const GRAY_100 = "#E1E1E1"
+export const GRAY_200 = "#CFCFCF"
+export const GRAY_300 = "#B1B1B1"
+export const GRAY_400 = "#9E9E9E"
+export const GRAY_500 = "#7E7E7E"
+export const GRAY_600 = "#626262"
+export const GRAY_700 = "#515151"
+export const GRAY_800 = "#3B3B3B"
+
+export const BLACK = "#000000DE"
