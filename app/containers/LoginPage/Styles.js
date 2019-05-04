b1c82bac3cbb867e5a924186e4cbdbf87b57e30a
Moritz Lang <mlang.mmt-b2016@fh-salzburg.ac.at>
2019-05-04 23:05:31 +0200

    feat(LoginPage): add LoginPage

---
+++ app/containers/LoginPage/Styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+
+export const Wrapper = styled.div`
+`
+
+export const Text = styled.h1`
+  margin-top: 0;
+`
---
+++ app/containers/LoginPage/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { Wrapper, Text } from './Styles'
+
+
+export class LoginPage extends React.PureComponent {
+
+  render() {
+    return (
+      <Wrapper>
+        <Text>Login Page</Text>
+      </Wrapper>
+    )
+  }
+}
+
+export default LoginPage
