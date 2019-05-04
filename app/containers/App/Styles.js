47b4379801dd5fa5ca90527689f3309543dc2b35
Moritz Lang <mlang.mmt-b2016@fh-salzburg.ac.at>
2019-05-04 23:15:15 +0200

    feat(App): setup add container and routes

---
+++ app/containers/App/Styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+import { APP_BAR_HEIGHT } from 'styles/dimensions'
+
+export const Wrapper = styled.div`
+  margin-top: ${APP_BAR_HEIGHT};
+  padding: 1.5em;
+`
--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -7,22 +7,52 @@
  *
  */
 
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 
-import HomePage from 'containers/HomePage/Loadable';
-import NotFoundPage from 'containers/NotFoundPage/Loadable';
+// import firebase from 'firebase'
+// import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
 
-import GlobalStyle from '../../global-styles';
+import JssProvider from 'react-jss/lib/JssProvider'
+import { create } from 'jss'
+import { createGenerateClassName, jssPreset } from '@material-ui/core/styles'
+
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
+
+import MenuAppBar from 'components/MenuAppBar'
+
+import HomePage from 'containers/HomePage/Loadable'
+import LoginPage from 'containers/LoginPage/Loadable'
+import NotFoundPage from 'containers/NotFoundPage/Loadable'
+import PrivateRoute from './PrivateRoute'
+import theme from './theme'
+
+import { Wrapper } from './Styles'
+
+import GlobalStyle from '../../global-styles'
+
+// This is for overriding material-ui styles with styled-components
+const generateClassName = createGenerateClassName()
+const jss = create({
+  ...jssPreset(),
+  // Define a custom insertion for injecting the JSS styles in the DOM
+  insertionPoint: document.getElementById('jss-insertion-point'),
+})
 
 export default function App() {
   return (
-    <div>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route component={NotFoundPage} />
-      </Switch>
-      <GlobalStyle />
-    </div>
-  );
+    <JssProvider jss={jss} generateClassName={generateClassName}>
+      <MuiThemeProvider theme={theme}>
+        <MenuAppBar />
+        <Wrapper>
+          <Switch>
+            <PrivateRoute exact path='/' component={HomePage} />
+            <Route path="/login" component={LoginPage} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </Wrapper>
+        <GlobalStyle />
+      </MuiThemeProvider>
+    </JssProvider>
+  )
 }
