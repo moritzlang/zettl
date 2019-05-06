1fba1c3c2e3066cb788c656792d616dde067c538
Moritz Lang <mlang.mmt-b2016@fh-salzburg.ac.at>
2019-05-06 04:06:19 +0200

    feat(typography): add typography styles

---
+++ app/styles/typography.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+import * as Color from 'styles/colors'
+import { maxWidth } from 'utils/device'
+
+export const PRIMARY_FONT = "'Helvetica Neue',Helvetica,Arial,sans-serif"
+export const SECONDARY_FONT = "'Helvetica Neue',Helvetica,Arial,sans-serif"
+
+export const MEDIUM = '400'
+export const SEMI_BOLD = '500'
+export const BOLD = '600'
+
+export const Headline = styled.h1`
+  font-family: ${PRIMARY_FONT};
+  font-weight: bold;
+  line-height: 46px;
+  font-size: 42px;
+  color: ${Color.BLACK};
+  white-space: pre-wrap;
+  margin-top: 0;
+
+  @media ${maxWidth.tablet} {
+    font-size: 32px;
+    line-height: 36px;
+  }
+`
+
+export const Introduction = styled.p`
+  font-family: ${PRIMARY_FONT};
+  font-style: normal;
+  font-weight: 400;
+  line-height: 30px;
+  font-size: 20px;
+  color: ${Color.GRAY_600};
+  white-space: pre-wrap;
+`
