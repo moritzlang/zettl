34452e6cb07c7fe2f6aedda02324106793a91ab9
Moritz Lang <mlang.mmt-b2016@fh-salzburg.ac.at>
2019-05-12 02:49:33 +0200

    feat(List): add selector to select lists in right format for react-select component

--- app/containers/List/selectors.js
+++ app/containers/List/selectors.js
@@ -8,7 +8,25 @@ const makeSelectLists = () => createSelector(
   (lists) => lists
 )
 
+const makeSelectListsOverview = () => createSelector(
+  selectLists, 
+  (lists) => {
+    if(lists) {
+      const arr = lists.get('lists').valueSeq().toArray().map((list) => (
+        {
+          value: list.get('id'),
+          label: list.get('title'),
+          owner: list.get('owner'),
+        }
+      ))
+      return arr
+    }
+    return lists
+  }
+)
+
 export {
   selectLists,
   makeSelectLists,
+  makeSelectListsOverview,
 }
