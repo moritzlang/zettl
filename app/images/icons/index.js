c4a8775400b831203027b02981977c49411c9c21
Moritz Lang <mlang.mmt-b2016@fh-salzburg.ac.at>
2019-05-12 02:32:35 +0200

    feat(icons): add group icon

--- app/images/icons/index.js
+++ app/images/icons/index.js
@@ -106,3 +106,15 @@ export const CloseIcon = props => {
     </SvgIcon>
   )
 }
+
+export const GroupIcon = props => {
+  const colorPrimary = props.colors && props.colors.primary ? props.colors.primary : Color.ICON_PRIMARY
+  const colorSecondary = props.colors && props.colors.secondary ? props.colors.secondary : Color.ICON_SECONDARY
+  
+  return (
+    <SvgIcon {...props}>
+      <path fill={colorPrimary} d="M12 13a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v3a1 1 0 0 1-1 1h-8a1 1 0 0 1-1-1 1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-3a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3zM7 9a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm10 0a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+      <path fill={colorSecondary} d="M12 13a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm-3 1h6a3 3 0 0 1 3 3v3a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1v-3a3 3 0 0 1 3-3z"/>
+    </SvgIcon>
+  )
+}
