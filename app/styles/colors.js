ad6ad7b7b5b9d58d92058dc5dfdf364da7b05b27
Moritz Lang <mlang.mmt-b2016@fh-salzburg.ac.at>
2019-05-12 18:02:02 +0200

    feat(colors): add new colors

--- app/styles/colors.js
+++ app/styles/colors.js
@@ -1,9 +1,16 @@
 // Primary
-export const BLUE_PRIMARY = "#2BB0ED"
+export const TEAL_050 = "#F0FCF9"
+export const TEAL_100 = "#C6F7E9"
+export const TEAL_200 = "#8EEDD1"
+export const TEAL_300 = "#5FE3C0"
+export const TEAL_400 = "#2DCCA7"
+export const TEAL_500 = "#17B897"
+export const TEAL_PRIMARY = "#17B897"
+export const TEAL_600 = "#079A82"
+export const TEAL_700 = "#048271"
+export const TEAL_800 = "#016457"
+export const TEAL_900 = "#004440"
 
-// Icons
-export const ICON_PRIMARY = "#A5B3BB"
-export const ICON_SECONDARY = "#0D2B3E"
 
 // Supporting
 export const RED_050 = "#FFE3E3"
@@ -11,24 +18,32 @@ export const RED_100 = "#FFBDBD"
 export const RED_200 = "#FF9B9B"
 export const RED_300 = "#F86A6A"
 export const RED_400 = "#EF4E4E"
+export const RED_500 = "#E12D39"
 export const RED_PRIMARY = "#E12D39"
 export const RED_600 = "#CF1124"
 export const RED_700 = "#AB091E"
 export const RED_800 = "#8A041A"
-export const RED_900 = "#610316"   
+export const RED_900 = "#610316"
 
-export const GREEN_050 = "#EFFCF6"
-export const GREEN_100 = "#C6F7E2"
-export const GREEN_200 = "#8EEDC7"
-export const GREEN_300 = "#65D6AD"
-export const GREEN_400 = "#3EBD93"
-export const GREEN_500 = "#27AB83"
-export const GREEN_600 = "#199473"
-export const GREEN_700 = "#147D64"
-export const GREEN_800 = "#0C6B58"
-export const GREEN_900 = "#014D40"
+export const YELLOW_050 = "#FFFBEA"
+export const YELLOW_100 = "#FFF3C4"
+export const YELLOW_200 = "#FCE588"
+export const YELLOW_300 = "#FADB5F"
+export const YELLOW_400 = "#F7C948"
+export const YELLOW_500 = "#F0B429"
+export const YELLOW_PRIMARY = "#F0B429"
+export const YELLOW_600 = "#DE911D"
+export const YELLOW_700 = "#CB6E17"
+export const YELLOW_800 = "#B44D12"
+export const YELLOW_900 = "#8D2B0B"
 
-// Neutral
+
+// Icons
+export const ICON_PRIMARY = "#A5B3BB"
+export const ICON_SECONDARY = "#0D2B3E"
+
+
+// Neutrals
 export const BACKGROUND_COLOR = '#F9F7F3'
 export const BACKGROUND_COLOR_SHADE = '#E3DED6'
 
