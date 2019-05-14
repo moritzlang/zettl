e8d548542670706abac3a9ed3c094ac88afc8e2e
Moritz Lang <mlang.mmt-b2016@fh-salzburg.ac.at>
2019-05-14 08:14:52 +0200

    fix(Firebase): remove Twitter from sign in options

--- app/containers/Firebase/index.js
+++ app/containers/Firebase/index.js
@@ -63,7 +63,6 @@ export default {
     signInOptions: [
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
       firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
       firebase.auth.GithubAuthProvider.PROVIDER_ID,
       firebase.auth.EmailAuthProvider.PROVIDER_ID,
     ],
