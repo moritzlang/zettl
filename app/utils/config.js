1dcc07d176a9153cfd87c553d7e6fc9bcd90a2b2
Moritz Lang <mlang.mmt-b2016@fh-salzburg.ac.at>
2019-05-14 06:12:27 +0200

    feat(config): add DEBUG constant

--- app/utils/config.js
+++ app/utils/config.js
@@ -1,1 +1,2 @@
-export const url = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://zettl-835a3.firebaseapp.com'
+export const DEBUG = process.env.NODE_ENV === 'development'
+export const URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://zettl-835a3.firebaseapp.com'
