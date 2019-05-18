660199d29c10bf47fb13db4fc4c3ba0ce9fe913c
Moritz Lang <mlang.mmt-b2016@fh-salzburg.ac.at>
2019-05-18 06:46:53 +0200

    fix(HomePage): center HomePage content

--- app/containers/HomePage/Styles.js
+++ app/containers/HomePage/Styles.js
@@ -1,11 +1,10 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
   > div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     width: 100%;
   }
 `
