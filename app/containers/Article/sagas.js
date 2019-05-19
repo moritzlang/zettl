d65d5ab9f4092cdbce8d3b8236d55559a00f2462
Moritz Lang <mlang.mmt-b2016@fh-salzburg.ac.at>
2019-05-19 14:44:01 +0200

    style(Article): remove unnecessary comments

--- app/containers/Article/sagas.js
+++ app/containers/Article/sagas.js
@@ -1,7 +1,6 @@
 import { eventChannel, END } from 'redux-saga'
 import { put, cancelled, call, take, fork } from 'redux-saga/effects'
 import Firebase from 'containers/Firebase'
-// import axios from 'axios'
 
 import {
   articlesLoaded,
@@ -36,7 +35,6 @@ export function* toggleArticle(payload) {
 
 export function* processArticle(payload) {
   try {
-    // yield fork(Firebase.updateArticle, articleId, { checked: value })
     yield put(articleProcessed(payload.data))
   } catch (err) {
     yield put(articlesProcessingError(err))
@@ -62,31 +60,7 @@ function createAddArticleChannel(article) {
 export function* addArticle(payload) {
   const newArticle = payload.data
   const channel = yield call(createAddArticleChannel, newArticle)
-
   try {
-    // const postData = {
-    //   data: {
-    //     title: 'Here is the test message',
-    //     body: payload.data,
-    //   },
-    //   to: 'frEK0Ts7b6Y:APA91bH7CpLXZWmxZCliQ2sjskT1Ho0DiZbAw5I6Alw8cc6KjhyElWC5KlO12lfkDUeZuEVgGgUmkQbb3GjSOl5ZYdJtwrp65BcoFhioyTjHeALf4b2NbpyQt9Gc1d-FV-TY6mwOjY9_',
-    // }
-    
-    // const axiosConfig = {
-    //   headers: {
-    //     'Content-Type': 'application/json;charset=UTF-8',
-    //     'Authorization': 'key=AAAAMSspG5o:APA91bGu3UzX5czt1FX7-v0QP-QAqOlVnqq432iQ1kxpgv5zG_r2AuMT44VU1FOD-ReF9QzowBk4qZ5KE19DY7N736BG2NH7SEOaW0-Th4Z1neWHLEGGFLRBq3c7HWtzSGMpHTlnC2uW',
-    //   },
-    // }
-    
-    // axios.post('https://fcm.googleapis.com/fcm/send', postData, axiosConfig)
-    //   .then((res) => {
-    //     console.log('RESPONSE RECEIVED: ', res)
-    //   })
-    //   .catch((err) => {
-    //     console.log('AXIOS ERROR: ', err)
-    //   })
-
     yield put(articleAdded(newArticle))
 
     // Wait for confirmation that article got
